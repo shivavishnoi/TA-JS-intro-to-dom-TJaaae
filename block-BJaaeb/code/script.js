9eca014c408821a53450e55ce8cc081a3a7c7efd
shivaji <shivavishnoi.vishnoi@gmail.com>
2022-04-17 10:31:18 +0530

    dom methods in function

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -6,15 +6,26 @@ default value to be "text" and return the input element inside label. (create it
 */
 
 // Your code goes here
-
+// let createInputElm = (label, type = "text") => {
+//   let parent = document.createElement(`label`)
+//   let child = document.createElement('input')
+//   child.setAttribute('type', type)
+//   parent.innerText = label
+//   parent.append(child)
+//   return parent
+// }
 // TEST
-createInputElm('Your name'); //<label>Your name: <input type="text"></label>
-createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
+// createInputElm('Your name'); //<label>Your name: <input type="text"></label>
+// createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
 
 // 2. Do the same thing as above using string literal like `<h1>Hello</h1>`
 
 // Your code goes here
-
+let createInputElm = (label, type = "text") => {
+  let parent = document.createElement(`label`)
+  parent.innerHTML = `${label}: <input type="${type}">`
+  return parent
+}
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
@@ -22,7 +33,15 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 // Your code goes here
-
+let createList = (array) => {
+  let ul = document.createElement('ul')
+  array.forEach((element, index) => {
+    let li = document.createElement('li')
+    li.innerText = element
+    ul.append(li)
+  });
+  return ul
+}
 // TEST
 createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
 createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
@@ -40,7 +59,28 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 */
 
 // Your code goes here
-
+let createTodoList = (array) => {
+  let ul = document.createElement(`ul`)
+  let li = document.createElement('li')
+  ul.append(li)
+  array.forEach((element) => {
+    if (!element.isDone) {
+      let p = document.createElement(`p`)
+      p.innerText = element.name
+      li.append(p)
+    }
+  })
+  let inp = document.createElement('input')
+  inp.setAttribute('checked', '')
+  inp.setAttribute('type', 'checkbox')
+  inp.setAttribute('name', ' ')
+  inp.setAttribute('id', ' ')
+  li.append(inp)
+  let span = document.createElement('span')
+  span.innerText = `X`
+  li.append(span)
+  return ul
+}
 // TEST
 createTodoList([
   { name: 'Learn DOM', isDone: false },
